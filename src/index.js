a2ed56290b6a43a4857d1a6ed642c8a6230e025e
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-04-30 11:02:41 +0200

    Uprava textu v galerii

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
+const navElm = document.querySelector('#nav-btn')
 
+navElm.addEventListener('click', ()=>{
+  const nav = document.querySelector('nav')
+nav.classList.toggle('nav-closed');
+})
